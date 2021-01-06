00115126ae150194045c26406c5c4bf332e7e591
Tim Remnant <timwebdesign2013@gmail.com>
2021-01-06 23:05:04 +0000

    Change border colour upon dark mode

--- src/components/photo.tsx
+++ src/components/photo.tsx
@@ -1,8 +1,24 @@
+import { useSelector } from "react-redux";
+import { themeStateSelector } from "../store/themeSlice";
+
 export const Photo = (): JSX.Element => {
+    
+    const usingDarkMode = useSelector(themeStateSelector);
+
+    const imageClassNames: string[] = [
+        "rounded-full",
+        "border-4"
+    ];
+
+    if (usingDarkMode) {
+        imageClassNames.push("border-white");
+    } else {
+        imageClassNames.push("border-black");
+    }
+
     return (
         <div id="photo" className="w-full lg:w-2/5 m-auto">
-            <img src="https://drive.google.com/uc?id=16o3IBFWACQ8SkVc6LuwMJyXabJqeFA7h" className="rounded-full has-white-border" alt="Me"/>
+            <img src="https://drive.google.com/uc?id=16o3IBFWACQ8SkVc6LuwMJyXabJqeFA7h" className={imageClassNames.join(' ')} alt="Me"/>
         </div>
-
     );
 }
