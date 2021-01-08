57c541838641e960b2a8cb16e7d39c9c5afc257d
Tim Remnant <timwebdesign2013@gmail.com>
2021-01-08 11:13:42 +0000

    Now storing image locally

--- src/components/photo.tsx
+++ src/components/photo.tsx
@@ -1,7 +1,9 @@
+import Me from '../assets/me.jpg';
+
 export const Photo = (): JSX.Element => {
     return (
         <div id="photo" className="w-full lg:w-2/5 mx-auto mt-4">
-            <img src="https://drive.google.com/uc?id=16o3IBFWACQ8SkVc6LuwMJyXabJqeFA7h" className="rounded-full border-4 border-black dark:border-white" alt="Me"/>
+            <img src={Me} className="rounded-full border-4 border-black dark:border-white" alt="Me"/>
         </div>
     );
 }
