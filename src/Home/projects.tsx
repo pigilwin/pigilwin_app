c28d4972da07f8ab5e5c826a303c2cd31893a418
Tim Remnant <timwebdesign2013@gmail.com>
2021-01-10 23:23:21 +0000

    Text Alignment

--- src/Home/projects.tsx
+++ src/Home/projects.tsx
@@ -1,6 +1,6 @@
 export const Projects = (): JSX.Element => {
     return (
-        <div className="grid sm:grid-cols-1 lg:grid-cols-4 gap-4 py-4 pt-0">
+        <div className="grid sm:grid-cols-1 lg:grid-cols-4 gap-4 p-4">
             <ProjectItem title="Synopsis" link="https://synopsis.pigilwin.com/#/"/>
             <ProjectItem title="Sounds" link="https://sounds.pigilwin.com/#/"/>
             <ProjectItem title="Blizzard" link="https://blizzard.pigilwin.com/#/"/>
@@ -14,8 +14,8 @@ interface ProjectItemProps {
 }
 const ProjectItem = ({title, link}: ProjectItemProps): JSX.Element => {
     return (
-        <div className="w-full m-2 px-4 py-8 rounded-lg shadow-lg bg-gray-500">
-            <a href={link} className="text-2xl font-bold text-center text-white hover:text-green-500">{title}</a>
+        <div className="w-full p-4 rounded-lg shadow-lg bg-gray-500 text-center">
+            <a href={link} className="text-2xl font-bold text-white hover:text-green-500">{title}</a>
         </div>
     );
 }
